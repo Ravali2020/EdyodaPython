a0dd8e8dabb7ec57b14ad0fe3d389996c40742da
Ravali2020 <60998616+Ravali2020@users.noreply.github.com>
2020-02-13 18:38:31 +0530

    Add files via upload

---
+++ edpython1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+for i in range(1,11):
+    for x in range(1,6):
+        print(i * x, end = '     ')
+    print(end='\n')
+
+
+# In[ ]:
+
+
+
+
